5e8c27bdb7759bde389b563710d44fe1920112c8
Djagadeesh4513 <jagadeeshdesetti27@gmail.com>
2025-09-15 23:39:29 +0530

    Second Commit

---
+++ src/Basics/Sum_of_Two_Numbers.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class Sum_of_Two_Numbers {
+    public static void main(String[] args) {
+        int a = 29;
+        int c = 69;
+        System.out.println(a+c);
+    }
+}
