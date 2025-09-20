15c18c7022ceaed807cf1d454460a9dfff53b042
Djagadeesh4513 <jagadeeshdesetti27@gmail.com>
2025-09-20 09:59:29 +0530

    Fourth Commit

---
+++ src/Basics/TC_04_BODMAS.java
@@ -0,0 +1,13 @@
+package Basics;
+
+import java.sql.SQLOutput;
+
+public class TC_04_BODMAS {
+    public static void main(String[] args) {
+        int a = ((10/2*5)*3);
+        System.out.println(a);
+
+        int b = ((10*2/5)*3);
+        System.out.println(b);
+    }
+}
