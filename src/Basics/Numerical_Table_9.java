1832f8f109a73662d8730706e744d9b5d0cb934f
Djagadeesh4513 <jagadeeshdesetti27@gmail.com>
2025-09-20 09:40:11 +0530

    Third Commit

---
+++ src/Basics/Numerical_Table_9.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class Numerical_Table_9 {
+    public static void main(String[] args) {
+        int num = 20;
+
+        for (int i = 1;i<=5;i++)
+        {
+            System.out.printf("20 * %d = %d",i,i*20).println();
+        }
+    }
+}
