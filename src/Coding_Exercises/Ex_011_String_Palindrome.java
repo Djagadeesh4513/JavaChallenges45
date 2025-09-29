0e8fcfab2e54b473736781d083b75367c377a5c5
Djagadeesh4513 <jagadeeshdesetti27@gmail.com>
2025-09-29 23:45:42 +0530

    09/29/2025

--- src/Coding_Exercises/Ex_011_String_Palindrome.java
+++ src/Coding_Exercises/Ex_011_String_Palindrome.java
@@ -2,7 +2,7 @@ package Coding_Exercises;
 
 import java.util.Scanner;
 
-public class Ex_011_String_Reversal {
+public class Ex_011_String_Palindrome {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
         String word = scr.nextLine();
@@ -13,7 +13,13 @@ public class Ex_011_String_Reversal {
             empty = empty + word.charAt(length);
             length--;
         }
-        System.out.println(empty);
-
+        if(word.equals(empty))
+        {
+            System.out.println("Palindrome");
+        }
+        else
+        {
+            System.out.println("Not a Palindrome");
+        }
     }
 }
