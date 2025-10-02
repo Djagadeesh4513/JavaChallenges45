f0d1be2a430722ebff10ebafe94c381bbe6ced09
Djagadeesh4513 <jagadeeshdesetti27@gmail.com>
2025-10-02 19:58:02 +0530

    10/02/2025

---
+++ src/Coding_Exercises/Ex_014_Vowels_Consonants.java
@@ -0,0 +1,22 @@
+package Coding_Exercises;
+public class Ex_014_Vowels_Consonants {
+    public static void main(String[] args) {
+        String sss = "Interview Questions";
+        int vow = 0;
+        int con = 0;
+        for(int i=1;i<=sss.length();i++)
+        {
+            char c = sss.charAt(i-1);
+            if (c==65||c==69||c==73||c==79||c==85||c==97||c==101||c==105||c==111||c==117)
+            {
+                vow++;
+            }
+            else
+            {
+                con++;
+            }
+        }
+        System.out.println("No. of vowels = "+vow);
+        System.out.println("No. of consonants = "+con);
+    }
+}
---
+++ src/Coding_Exercises/Ex_015_FibonacciSeries.java
@@ -0,0 +1,35 @@
+package Coding_Exercises;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class Ex_015_FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number in the Fibonacci series...");
+
+        if(!input.hasNextInt())
+        {
+            System.out.println("Only +ve Integer values are allowed");
+        }
+
+        int ab = input.nextInt();
+        System.out.println("Enter the second number in the Fibonacci series...");
+        int bc = input.nextInt();
+        System.out.println("Enter no. of values u want to print");
+        int cnt = input.nextInt();
+
+        int sum = 0;
+        System.out.println(ab);
+        System.out.println(bc);
+        for(int i=1;i<=cnt-2;i++)
+        {
+            sum = ab + bc;
+            ab = bc;
+            bc = sum;
+
+            System.out.println(sum);
+        }
+
+    }
+}
